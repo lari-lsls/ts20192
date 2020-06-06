9b269c9d243cfec698b8e2e72fc050b48a18920f
lari-lsls <larissasoares.lsls@gmail.com>
2020-06-06 08:10:07 -0300

    basic structure

---
+++ src/test/java/runner/Runner.java
@@ -0,0 +1,12 @@
+package runner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+)
+public class Runner {
+}
